ff52556fe9b51f8e4cf953069b3621756cda3b94
Tobias Bech Knudsen <tok005@edu.zealand.dk>
2024-12-12 13:14:21 +0100

    hehehhe

--- EksamenProjekt-1web/MockData/MockSag.cs
+++ EksamenProjekt-1web/MockData/MockSag.cs
@@ -2,7 +2,9 @@
 {
     public class MockCases
     {
-        public static List<Sag> Sager { get; }
+        public static List<Sag> Sag { get; }
+        public static List<Kunde> Kunder { get; }
 
+        new Sag("12rd", 2012, "Citreon C1", "dn 55155");
     }
 }
