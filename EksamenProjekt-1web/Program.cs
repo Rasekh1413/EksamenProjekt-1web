b67547d3062e80dcf6893af7d91f90b3f8f23fde
Rasekh <rasekh1413@icloud.com>
2024-12-18 21:35:26 +0100

    r5

--- EksamenProjekt-1web/Program.cs
+++ EksamenProjekt-1web/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// den her ned hvis man har ikke tilføj det, så crasher sager og kunde knappen.
 builder.Services.AddScoped<ISagRepository, SagRepository>();      // Tilføj fra Rasekh
 builder.Services.AddScoped<IKundeRepository, KundeRepository>();  // Tilføj fra Rasekh
 
